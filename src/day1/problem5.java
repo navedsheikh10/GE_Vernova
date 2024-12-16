ba44fe64294de004ab5b420de67b753ce6b6c08c
navedsheikh10 <navedsheikh2299@gmail.com>
2024-12-17 02:53:40 +0530

    folder created

---
+++ src/day1/problem5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class problem5 {
+    // Initialize the Variables
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter a number to find its prime factors: ");
+        int number = input.nextInt();
+
+        // Display prime factors
+        System.out.println("The prime factors of " + number + " are:");
+
+        int divisor = 2;
+        while (number > 1) {
+            if (number % divisor == 0) {
+                System.out.print(divisor + " ");
+                number /= divisor;
+            } else {
+                divisor++;
+            }
+        }
+    }
+}
